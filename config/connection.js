524371aaa04a18947e48eb0c844d93cf89572d2b
olivialee <oliviasylee@gmail.com>
2023-02-04 13:40:17 -0500

    Defined columns for category and product in models

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-
+// JAWSDB_URL is an environment variable that contains the connection string for a database hosted on JawsDB, which is a fully managed MySQL database service for use with applications hosted on the Heroku platform.  
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+// Tag - product's feature such as color, what genre of the music? 
+Product.belongsToMany(Tag, {
+
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+
+
+})
+
 
 module.exports = {
   Product,
