8f4a64023aeae10988126d64302befd417daed00
olivialee <oliviasylee@gmail.com>
2023-02-05 12:52:03 -0500

    Finished DELETE product route by its `id`

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -76,7 +76,7 @@ router.delete('/:id', async (req, res) => {
     if (!categoryData) {
       res.status(404).json({
       message: 'No category found with that id!'});
-      return
+      return;
     }
     res.status(200).json(categoryData);
   } catch (err) {
