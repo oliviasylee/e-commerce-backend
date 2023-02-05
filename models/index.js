065ba6a91c4cc87bd6a8cb23b5df144cb140d01e
olivialee <oliviasylee@gmail.com>
2023-02-04 19:37:26 -0500

    Created endpoint for retrieving all categories

--- models/index.js
+++ models/index.js
@@ -4,32 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-
-
-
-})
-
+// one-to-many
 // Categories have many Products
 Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // When we delete a category, make suere to also delete the associated product.
+  // 카테고리가 사라지면 그에 속한 제품들이 전부 공중에 뜨니까? 데이터가 연관되어 있으니 카테고리가 삭제되면 관련된 제품도 삭제되어야 함. 하지만 반대는 안됨. 제품이 삭제되어도 카테고리는 그대로 남아있어야 함 아!
+  onDelete: 'CASCADE',
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 
-})
-
+// many-to-many
 // Products belongToMany Tags (through ProductTag)
 // Tag - product's feature such as color, what genre of the music? 
+// ProductTag is junction table that will keep track of the associations will be called ProductTag, which will contain the foreign keys product_id and tag_id
 Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
-
-})
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-
-
-})
-
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// GET all categories
+// find all categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+      const categoryData = await Category.findAll({
+        include: [{ model: Product }],
+      });
+      res.status(200).json(categoryData);
+    } catch (err) {
+    res.status(500).json(err);
+  };
+
+// GET a singer category
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product }],
+      });
+
+      if (!categoryData) {
+        res.status(404.).json({ message: 'No category found with that id!'});
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
