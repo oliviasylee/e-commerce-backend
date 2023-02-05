56b41b5b1fb3c1f5da47981a24da0b298fac8fd5
olivialee <oliviasylee@gmail.com>
2023-02-05 12:44:41 -0500

    Created GET all products and one product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// GET all products
+// find all products
+// be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      // 태그 DB의 태그_네임 밑에 ProductTag가 또 종속되어야하는데 
+      // Nested eager loading to load all related models of a related model
+      include: [
+        { model: Category }, 
+        { model: Tag, 
+          include: { model: ProductTag },
+        }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try{
+    const productData = await Product.findByPk(req.param.id, {
+      include: [
+        { model: Category }, 
+        { model: Tag, 
+          include: { model: ProductTag },
+        }],
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -89,8 +115,9 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  
 });
 
 module.exports = router;
